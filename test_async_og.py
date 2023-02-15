ac5ff08a18de6037cc5651fc0b4e3f58d628b2d9
Melissa Hazlewood <melissa.hazlewood@bionautlabs.com>
2023-02-14 17:21:56 -0800

    t1 test get_camera with None and camera id as inputs, checking camera id is expected value

--- test_async_og.py
+++ test_async_og.py
@@ -1,6 +1,15 @@
 import pytest
+from async_og import get_camera
+
+CAM_ID = "DEV_1AB228000FA4"
 
 class TestAsyncOG:
-    def test_pytest_works(self):
-        pass
+    def test_when_given_none_as_input_get_camera_returns_camera_with_expected_id(self):
+        cam = get_camera(None)
 
+        assert cam.get_id() == CAM_ID
+
+    def test_get_camera_returns_camera_with_expected_id(self):
+        cam = get_camera(CAM_ID)
+        
+        assert cam.get_id() == CAM_ID
