ce0396c8ae64f46b44d79fb83beb2c0350642998
Melissa Hazlewood <melissa.hazlewood@bionautlabs.com>
2023-02-14 18:15:34 -0800

    r1 split tests by whether camera is connected or not

--- test_async_og.py
+++ test_async_og.py
@@ -1,15 +1,21 @@
 import pytest
-from async_og import get_camera
+from async_og import *
 
 CAM_ID = "DEV_1AB228000FA4"
 
-class TestAsyncOG:
-    def test_when_given_none_as_input_get_camera_returns_camera_with_expected_id(self):
+class TestAsyncOGWithCameraConnected:
+    def test_given_none_as_input_get_camera_returns_a_camera(self):
         cam = get_camera(None)
 
+        assert cam is not None
+
+    def test_given_camera_id_get_camera_returns_camera_with_expected_id(self):
+        cam = get_camera(CAM_ID)
+
         assert cam.get_id() == CAM_ID
 
-    def test_get_camera_returns_camera_with_expected_id(self):
-        cam = get_camera(CAM_ID)
-        
-        assert cam.get_id() == CAM_ID
+
+class TestAsyncOGNoCameraConnected:
+    def test_get_camera_does_something_still(self):
+        cam = get_camera(None)
+        pass
