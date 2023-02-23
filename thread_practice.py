a4356798917222928b422c24fc003bf1dfbcd6ee
Melissa Hazlewood <melissa.hazlewood@bionautlabs.com>
2023-02-23 15:57:00 -0800

    r1 can list times for processing frames since start time
    Co-authored-by: Cyrus Metcalf <cmetcalf-bionaut@users.noreply.github.com>

--- thread_practice.py
+++ thread_practice.py
@@ -1,10 +1,13 @@
 from vimba import *
 import threading
 import cv2
+from datetime import datetime
 
 class Handler:
     def __init__(self):
         self.shutdown_event = threading.Event()
+        self.frame_time = 0
+        self.frame_times = []
 
     def __call__(self, cam: Camera, frame: Frame):
         NO_KEY_PRESS_CODE = -1
@@ -13,6 +16,8 @@ class Handler:
             self.shutdown_event.set()
             return
         elif frame.get_status() == FrameStatus.Complete:
+            self.frame_time = datetime.now() - start_time
+            self.frame_times.append(self.frame_time)
             print('{} acquired {}'.format(cam, frame), flush=True)
 
             msg = 'Stream from \'{}\'. Press <Enter> to stop stream.'
@@ -25,13 +30,19 @@ def frame_handler(cam, frame):
     cam.queue_frame(frame)
 
 def main():
+    global start_time
+
     with Vimba.get_instance() as vimba:
         cams = vimba.get_all_cameras()
         with cams[0] as cam:
             handler = Handler()
             try:
+                start_time = datetime.now()
                 cam.start_streaming(handler=handler, buffer_count=10)
                 handler.shutdown_event.wait()
+
+                average_time = handler.frame_times
+                [print(str(avg)) for avg in average_time]
             finally:
                 cam.stop_streaming()
             
