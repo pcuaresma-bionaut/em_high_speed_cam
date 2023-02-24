bf0c3a08dfcb5278634bc98a98b3f975dc0f8e4b
Melissa Hazlewood <melissa.hazlewood@bionautlabs.com>
2023-02-24 14:29:56 -0800

    f1 can calculate frequency

--- thread_practice.py
+++ thread_practice.py
@@ -10,14 +10,22 @@ class Handler:
         self.frame_times = []
 
     def __call__(self, cam: Camera, frame: Frame):
+        global start_frame_time
+        global end_frame_time
+
+        start_frame_time = datetime.now()
+
         NO_KEY_PRESS_CODE = -1
         key = cv2.waitKey(1)
+
         if key != NO_KEY_PRESS_CODE:
             self.shutdown_event.set()
             return
         elif frame.get_status() == FrameStatus.Complete:
-            self.frame_time = datetime.now() - start_time
+            end_frame_time = datetime.now()
+            self.frame_time = end_frame_time - start_frame_time
             self.frame_times.append(self.frame_time)
+
             print('{} acquired {}'.format(cam, frame), flush=True)
 
             msg = 'Stream from \'{}\'. Press <Enter> to stop stream.'
@@ -30,19 +38,25 @@ def frame_handler(cam, frame):
     cam.queue_frame(frame)
 
 def main():
-    global start_time
+    global start_frame_time
+    global end_frame_time
 
     with Vimba.get_instance() as vimba:
         cams = vimba.get_all_cameras()
         with cams[0] as cam:
             handler = Handler()
             try:
-                start_time = datetime.now()
                 cam.start_streaming(handler=handler, buffer_count=10)
                 handler.shutdown_event.wait()
 
-                average_time = handler.frame_times
-                [print(str(avg)) for avg in average_time]
+                frame_times_in_seconds = [time.total_seconds() for time in handler.frame_times]
+                [print(str(time)) for time in frame_times_in_seconds]
+                total_time_in_seconds = sum(frame_times_in_seconds)
+                print(f"Total time (s): {total_time_in_seconds}")
+                average_time_in_seconds = total_time_in_seconds / len(frame_times_in_seconds)
+                print(f"Average time (s): {average_time_in_seconds}")
+
+                print(f"Frequency (1/AverageTime): {1/average_time_in_seconds}")
             finally:
                 cam.stop_streaming()
             
