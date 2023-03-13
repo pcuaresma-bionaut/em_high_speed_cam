592840211f56786a9198b1d9bc2dc80d4e04449a
Melissa Hazlewood <melissa.hazlewood@bionautlabs.com>
2023-03-13 09:58:09 -0700

    add command line functionality and refactored

--- run_camera.py
+++ run_camera.py
@@ -10,6 +10,7 @@ import time
 import sys
 import argparse
 import getopt
+import logging
 
 """
 TODO:
@@ -33,63 +34,93 @@ TODO:
         > name_video.avi
 """
 
-OUTPUT_STR = "test_output"
-
-def configure_save_settings():
-    args = get_parsed_arguments()
-    OUTPUT_STR = args.file_string
-    DELETE_FRAMES = args.video_only
-    print(f"file string? {OUTPUT_STR}")
-    print(f"delete frames? {DELETE_FRAMES}")
-
-def get_parsed_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file_string", default="test_output", help="string to be used in output directory name, output video file name, and output frame image file names. Purpose: to identify the camera run the output originates from.")
-    parser.add_argument("-v", "--video-only", action='store_true', default=False, help="if true, stores the video only (no frames); defaults to False")
-    args = parser.parse_args(sys.argv[1:])
-    return args
-
-timestr = time.strftime("%Y%m%d-%H%M%S")
-    
-OUTPUT_DIR = os.path.join(os.path.dirname(__file__), OUTPUT_STR + "/")
-VIDEO_NAME = OUTPUT_STR + "_video.avi"
-FILE_NAME = OUTPUT_STR + "_image_{:0>6}.jpg" 
 FPS = 475
 
-def prepare_output_folder():
-    try:
-        os.makedirs(OUTPUT_DIR)
-    except FileExistsError:
-        delete_all_files_in(OUTPUT_DIR)
-        pass
+class IOStuff:
+    def __init__(self):
+        self.output_str, self.delete_frames = self.configure_save_settings()
+        self.output_dir = self.prepare_output_folder()
+        self.video_name = self.output_str + "_video.avi"
+        self.file_name = self.output_str + "_image_{:0>6}.jpg"
+        
+    def configure_save_settings(self):
+        args = self.get_parsed_arguments()
+        return args.file_string, args.video_only
+
+    def get_parsed_arguments(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("file_string", default="test_output", help="string to be used in output directory name, output video file name, and output frame image file names. Purpose: to identify the camera run the output originates from.")
+        parser.add_argument("-v", "--video-only", action='store_true', default=False, help="if true, stores the video only (no frames); defaults to False")
+        args = parser.parse_args(sys.argv[1:])
+        return args
+
+    def prepare_output_folder(self):
+        output_folder = os.path.join(os.path.dirname(__file__), self.output_str + "/")
+        try:
+            os.makedirs(output_folder)
+        except FileExistsError:
+            self.delete_all_files_in(output_folder)
+            pass
+        return output_folder
+
+    def delete_all_files_in(self, folder):
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+    def write_frames_to_video(self):
+        logging.info(f"Saving frames as video to: {self.output_str}/...")
+        images = [img for img in os.listdir(self.output_dir) if img.endswith(".jpg")]
+        images.sort()
+
+        frame = cv2.imread(os.path.join(self.output_dir, images[0]))
+        height, width, _ = frame.shape
+
+        video = cv2.VideoWriter(self.video_name, 0, FPS, (width,height))
+
+        for image in images:
+            video.write(cv2.imread(os.path.join(self.output_dir, image)))
+
+        cv2.destroyAllWindows()
+        video.release()
+        
+        if self.delete_frames:
+            logging.info("Deleting frames...")
+            for f in os.listdir(self.output_dir):
+                if not f.endswith(".jpg"):
+                    continue
+                os.remove(os.path.join(self.output_dir, f))
+        logging.info("Done.")
 
 class ImageFunctions:
     def show(frame_image, cam):
         msg = 'Stream from \'{}\'. Press <Enter> to stop stream.'
         cv2.imshow(msg.format(cam.get_name()), frame_image)
 
-    def show_and_save(frame, cam):
+    def show_and_save(frame, cam, io_stuff):
         frame_image = frame.as_opencv_image()
         ImageFunctions.show(frame_image,cam)
 
-        os.chdir(OUTPUT_DIR)
-        cv2.imwrite(FILE_NAME.format(frame.get_id()), frame_image)
+        os.chdir(io_stuff.output_dir)
+        cv2.imwrite(io_stuff.file_name.format(frame.get_id()), frame_image)
 
 class FrameHandler:
-    def __init__(self):
-        # Video/Streaming Fields
+    def __init__(self, io_stuff):
         self.shutdown_event = threading.Event()
-
-        prepare_output_folder()
-
-        # Timing/Frame Rate Fields
         self.timestamps = []
+        self.io_stuff = io_stuff
 
     def handle_frame(self, cam, frame):
         if frame.get_status() == FrameStatus.Complete:
             self.timestamps.append(frame.get_timestamp())
             print(f"{cam} acquired {frame}", flush=True)
-            ImageFunctions.show_and_save(frame, cam)
+            ImageFunctions.show_and_save(frame, cam, self.io_stuff)
 
     def __call__(self, cam: Camera, frame: Frame):
         """A callback function that is called for every frame received from the camera."""
@@ -105,38 +136,6 @@ class FrameHandler:
 
 def setup_camera(camera):
     camera.load_settings("settings_550fps.xml", PersistType.All)
-
-def delete_all_files_in(folder):
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-def write_frames_to_video():
-    images = [img for img in os.listdir(OUTPUT_DIR) if img.endswith(".jpg")]
-    images.sort()
-
-    frame = cv2.imread(os.path.join(OUTPUT_DIR, images[0]))
-    height, width, _ = frame.shape
-
-    video = cv2.VideoWriter(VIDEO_NAME, 0, FPS, (width,height))
-
-    for image in images:
-        video.write(cv2.imread(os.path.join(OUTPUT_DIR, image)))
-
-    cv2.destroyAllWindows()
-    video.release()
-    
-    if DELETE_FRAMES:
-        for f in os.listdir(OUTPUT_DIR):
-            if not f.endswith(".jpg"):
-                continue
-            os.remove(os.path.join(mydir, f))
                 
 def calculate_frame_rate(time_diffs):
     """
@@ -158,19 +157,18 @@ def print_camera_features(cam):
         print(feature)
 
 def main():
-    configure_save_settings()
+    logging.getLogger().setLevel(logging.INFO) # Uncomment to enable info-level logging
+    io_stuff = IOStuff()
 
     with Vimba.get_instance() as vimba:
         cams = vimba.get_all_cameras()
         with cams[0] as cam:
             setup_camera(cam)
-
-            handler = FrameHandler()
+            handler = FrameHandler(io_stuff)
             try:
                 start = datetime.now()
                 cam.start_streaming(handler=handler, buffer_count=10)
                 handler.shutdown_event.wait()
-
             finally:
                 cam.stop_streaming()
                 end = datetime.now()
@@ -178,7 +176,7 @@ def main():
     print(f"Total time streaming: {(end-start).total_seconds()} seconds")
 
     print_frame_rate_calculated_using_vimba_timestamps(handler)
-    write_frames_to_video()
+    io_stuff.write_frames_to_video()
 
 if __name__ == '__main__':
     main()
